97fda49e581832a611692fb08531e321c7e8d6a1
Simeon Franklin <sfranklin@twitter.com>
2015-08-09 17:52:04 -0700

    Add ModelAdmin class to customize admin display

--- mybooks/books/admin.py
+++ mybooks/books/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'read_on'
+    list_display = ('title', 'isbn', 'read_on')
+
+admin.site.register(Book, BookAdmin)
